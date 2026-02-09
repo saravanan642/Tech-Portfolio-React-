0ca778473bee04966aa7144683b68efb5030fd59
saravanan642 <saravanansenthil605@gmail.com>
2026-02-09 16:05:48 +0530

    update header.js

--- src/Compoents/Header.js
+++ src/Compoents/Header.js
@@ -1,24 +1,29 @@
 import img1 from "../Assests/saravanan.jpg";
 
-
 const Header = () => {
     return (
         <div>
-            <div> <img
-                src={img1}
-                alt="Profile"
-                className="w-28 h-28 rounded-full object-cover"
-            />
-            <div className=" text-white font-medium flex gap-8 font-serif ">
-                <a>HOME</a>
-                <a>ABOUT</a>
-                <a>PROJECTS</a>
-                <a>CONTACT</a>
-                <a>EXPOSURE</a>
-            </div>
-            
-            <div></div>
-            
+            <div className=" flex  justify-around mt-9">
+                <img
+                    src={img1}
+                    alt="Profile"
+                    className="w-28 h-28 rounded-full object-cover"
+                />
+                <div className=" m-10 flex gap-14">
+                    <div className=" text-white font-medium flex gap-14 font-serif ">
+                        <a>HOME</a>
+                        <a>ABOUT</a>
+                        <a>PROJECTS</a>
+                        <a>CONTACT</a>
+                        <a>EXPOSURE</a>
+                    </div>
+
+                    <div >
+                        <a className=" text-white font-medium flex gap-8  ml-11 font-serif">SKILLS</a>
+                    </div>
+                </div>
+
+
             </div>
 
 
