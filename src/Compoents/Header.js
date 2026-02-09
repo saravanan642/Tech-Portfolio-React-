06b61ab5bb3598865e9f0ca898e12aed6ce7ae27
saravanan642 <saravanansenthil605@gmail.com>
2026-02-09 16:24:21 +0530

    update header.js

--- src/Compoents/Header.js
+++ src/Compoents/Header.js
@@ -1,5 +1,5 @@
 import img1 from "../Assests/saravanan.jpg";
-
+import { IoIosKeypad } from "react-icons/io";
 const Header = () => {
     return (
         <div>
@@ -7,10 +7,10 @@ const Header = () => {
                 <img
                     src={img1}
                     alt="Profile"
-                    className="w-28 h-28 rounded-full object-cover"
+                    className="w-28 h-28 rounded-full object-cover ml-36 mt-2"
                 />
                 <div className=" m-10 flex gap-14">
-                    <div className=" text-white font-medium flex gap-14 font-serif ">
+                    <div className=" text-white font-medium flex gap-24 font-serif mr-30 pr-20 ">
                         <a>HOME</a>
                         <a>ABOUT</a>
                         <a>PROJECTS</a>
@@ -18,8 +18,12 @@ const Header = () => {
                         <a>EXPOSURE</a>
                     </div>
 
-                    <div >
-                        <a className=" text-white font-medium flex gap-8  ml-11 font-serif">SKILLS</a>
+                    <div  className=" flex text-white  gap-3 ">
+                        <div> <IoIosKeypad className=" object-cover w-7 h-7  pb-1" /></div>
+                        <a className=" text-white font-medium flex gap-8  font-serif">SKILLS</a>
+
+
+
                     </div>
                 </div>
 
