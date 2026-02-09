cf12c1eb82dac4bea0fda3feca81a9c46e8cc06e
saravanan642 <saravanansenthil605@gmail.com>
2026-02-09 15:47:18 +0530

    update header.js

--- src/Compoents/Header.js
+++ src/Compoents/Header.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import img1 from "../Assests/saravanan.jpg";
+
 
 const Header = () => {
-  return (
-    <div>
-      <p className=' text-gray-950'>saravanan</p>
-    </div>
-  )
+    return (
+        <div>
+            <div> <img
+                src={img1}
+                alt="Profile"
+                className="w-28 h-28 rounded-full object-cover"
+            />
+            <div className=" text-white font-medium flex gap-8 font-serif ">
+                <a>HOME</a>
+                <a>ABOUT</a>
+                <a>PROJECTS</a>
+                <a>CONTACT</a>
+                <a>EXPOSURE</a>
+            </div>
+            
+            <div></div>
+            
+            </div>
+
+
+        </div>
+    )
 }
 
 export default Header
