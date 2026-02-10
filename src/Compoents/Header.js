8c229a30ea8df0c611a0007a46090ff1772cdb6f
saravanan642 <saravanansenthil605@gmail.com>
2026-02-10 13:40:14 +0530

    update header.js

--- src/Compoents/Header.js
+++ src/Compoents/Header.js
@@ -3,24 +3,23 @@ import { IoIosKeypad } from "react-icons/io";
 const Header = () => {
     return (
         <div>
-            <div className=" flex  justify-around mt-9">
+            <div className="  md:flex  justify-around mt-9 md:">
                 <img
                     src={img1}
                     alt="Profile"
-                    className="w-28 h-28 rounded-full object-cover ml-36 mt-2"
+                    className=" md:w-[100px] h-[100px] rounded-full object-cover ml-36 mt-2 border border-solid border-white border-[1px]  hover:scale-105 transition duration-300 ease-in-out"
                 />
-                <div className=" m-10 flex gap-14">
-                    <div className=" text-white font-medium flex gap-24 font-serif mr-30 pr-20 ">
-                        <a>HOME</a>
-                        <a>ABOUT</a>
-                        <a>PROJECTS</a>
-                        <a>CONTACT</a>
-                        <a>EXPOSURE</a>
+                <div className="md: m-8 flex gap-14 mr-40 ">
+                    <div className=" text-white  font-medium flex gap-24 font-serif mr-30 pr-20  border border-solid border-white  rounded-2xl p-3  pt-5 justify-evenly pl-16   ">
+                        <a className=" cursor-pointer  hover:text-cyan-400 transform hover:scale-105 transition  duration-300 ease-out" >HOME</a>
+                        <a className="  cursor-pointer  hover:text-cyan-400 transform hover:scale-105 transition  duration-300 ease-out">ABOUT</a>
+                        <a className=" cursor-pointer  hover:text-cyan-400 transform hover:scale-105 transition  duration-300 ease-out ">PROJECTS</a>
+                        <a className=" cursor-pointer  hover:text-cyan-400 transform hover:scale-105 transition  duration-300 ease-out">EXPOSURE</a>
                     </div>
 
-                    <div  className=" flex text-white  gap-3 ">
-                        <div> <IoIosKeypad className=" object-cover w-7 h-7  pb-1" /></div>
-                        <a className=" text-white font-medium flex gap-8  font-serif">SKILLS</a>
+                    <div className=" md: flex text-white  gap-3  mt-0 ml-11 border border-solid border-white rounded-2xl  p-3 pt-5 ">
+                        <div> <IoIosKeypad className=" object-cover w-7 h-7  pb-1  hover:text-cyan-400 transform hover:scale-110 transition  duration-300 ease-out"  /></div>
+                        <a className=" text-white font-medium flex gap-8  font-serif cursor-pointer  hover:text-cyan-400 transform hover:scale-105 transition  duration-300 ease-out">SKILLS</a>
 
 
 
